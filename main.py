488ec1c958c263ce64aab6caa0e9e542071546f3
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-06 22:20:08 +0530

    replaced mongo DB with sqlite3, added get_db(), close_db(), check_student_exists() functions

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from flask import render_template, request, redirect, url_for, Flask
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
+from flask import render_template, request, Flask, g
+import sqlite3
 
-load_dotenv()
-MONGODB_URI = os.environ.get("MONGODB_URI")
-client = MongoClient(MONGODB_URI)
-db = client.dalton
-student_info = db.studentInfo
-
+#cursor = conn.cursor()
+DATABASE = 'lmaeo.db'
 app = Flask(__name__)
 
 def get_initials(string):
     words = string.split()
     initials = [word[0].upper() for word in words]
     return ''.join(initials)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def check_student_exists(table_name, student_id):
+    db = get_db()
+    cursor = db.cursor()
+
+    sql = f"SELECT 1 FROM {table_name} WHERE student_id = ?"
+    cursor.execute(sql, (student_id,))
+    row = cursor.fetchone()
+    if row is not None:
+        return True
+    else:
+        return False
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -38,26 +56,26 @@ def add_students():
 
 @app.route('/data/', methods=['POST', 'GET'])
 def data():
+    db = get_db()
+    cursor = db.cursor()
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-
     elif request.method == 'POST':
         form_data = dict(request.form)  # form data from /addStudents
         
         stream_initials = get_initials(form_data.get('stream').title())
         name_initials = get_initials(form_data.get('student name'))
         student_id = stream_initials + name_initials + form_data.get('year1') + form_data.get('year2') + form_data.get('roll number')
-        print(student_id)
-        form_data['student_id'] = student_id # add student_id in the form
-        student_info.insert_one(dict(form_data))
-
-        # check if the data is successfully saved
-        result = student_info.find_one({'student_id':student_id})
-        if result:
-            status = True
+        # if student_id doesn't exist yet, creates a column with the given data
+        if check_student_exists(table_name='studentInfo',student_id=student_id) == False:
+            sql_insert = "INSERT INTO studentInfo (student_id, student_name, roll_no, stream, academic_year_from, academic_year_to) VALUES (?, ?, ?, ?, ?, ?)"
+            data = (student_id, form_data.get('student name'), int(form_data.get('roll number')), form_data.get('stream'),int(form_data.get('year1')), int(form_data.get('year2')))
+            cursor.execute(sql_insert, data)
+            db.commit()
         else:
-            status = False
-
+            return render_template('student_already_exists.html')
+        # check if the data is successfully saved
+        status = check_student_exists(table_name='studentInfo',student_id=student_id)
         return render_template('data.html', form_data=form_data, status=status)
 
 
