da235d763d38934a42d8d7d542a9db3993e8b8c3
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-12 03:07:35 +0530

    added db as a parameter instead of creating its own connection to the db

--- utils/check_student_exists.py
+++ utils/check_student_exists.py
@@ -1,14 +1,11 @@
 import sqlite3
 
-#cursor = conn.cursor()
-DATABASE = 'dalton.db'
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-def check_student_exists(table_name, student_id):
+def check_student_exists(db,table_name, student_id):
     """
     This function takes table_name and student_id as the parameters
     and checks if the given student_id exists in the table .
     """
+    cursor = db.cursor()
     sql = f"SELECT 1 FROM {table_name} WHERE student_id = ?"
     cursor.execute(sql, (student_id,))
     row = cursor.fetchone()
