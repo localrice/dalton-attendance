2350fa497c9f72ee3e6b8c837d2956345588ccd6
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-12 02:59:29 +0530

    added info about the check_student_exists() function

--- utils/check_student_exists.py
+++ utils/check_student_exists.py
@@ -5,6 +5,10 @@ DATABASE = 'dalton.db'
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
 def check_student_exists(table_name, student_id):
+    """
+    This function takes table_name and student_id as the parameters
+    and checks if the given student_id exists in the table .
+    """
     sql = f"SELECT 1 FROM {table_name} WHERE student_id = ?"
     cursor.execute(sql, (student_id,))
     row = cursor.fetchone()
