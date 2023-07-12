d27591377705aae3cdc0a477e1b79a1962289e91
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-13 01:30:16 +0530

    added query to add new table called dailyAttendance to store date, present and absent student ids

--- create_db.py
+++ create_db.py
@@ -4,13 +4,24 @@ conn = sqlite3.connect('dalton.db')
 cursor = conn.cursor()
 
 sql = '''
-CREATE TABLE StudentInfo (
+CREATE TABLE IF NOT EXISTS StudentInfo (
     student_id TEXT PRIMARY KEY, 
     student_name TEXT,
     roll_no INTEGER, 
     stream TEXT, 
     academic_year_from INTEGER,
     academic_year_to INTEGER
+)
+'''
+
+cursor.execute(sql)
+conn.commit()
+
+sql = '''
+CREATE TABLE IF NOT EXISTS dailyAttendance (
+    date TEXT,
+    present TEXT,
+    absent TEXT
 )
 '''
 
