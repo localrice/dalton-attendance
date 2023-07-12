705f8386ac9dc0c4a308db0e133442bc7d22135e
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-13 00:51:44 +0530

    updated the sort_dict_by_id() func to filter out the initials of the streams directly

--- utils/sort_dict.py
+++ utils/sort_dict.py
@@ -1,9 +1,7 @@
 def sort_dict_by_id(dictionary):
     """
     This function takes a dictionary as the parameter and arranges each key in ascending order 
-    according to the number that comes after the initial letter of the stream.
-
-    The academic year must be removed from the student_id before passing it as a key
+    according to the number that comes after the initial letter of the stream(A or S or C)
     """
-    sorted_dict = dict(sorted(dictionary.items(), key=lambda x: int(''.join(filter(str.isdigit, x[0])))))
+    sorted_dict = dict(sorted(dictionary.items(), key=lambda x: int(''.join(filter(str.isdigit, x[0].lstrip('ASC'))))))
     return sorted_dict
