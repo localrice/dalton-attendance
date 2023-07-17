37433c5e7fa9a38b0ac4c36effd35219ff371bb7
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-17 23:21:08 +0530

    added a new column named phone_numbers

--- create_db.py
+++ create_db.py
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS StudentInfo (
     student_name TEXT,
     roll_no INTEGER, 
     stream TEXT, 
+    phone_number INTEGER,
     academic_year_from INTEGER,
     academic_year_to INTEGER
 )
