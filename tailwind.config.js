e5a288e13584f7bb7998afcfb8897f08cc6ebb49
Prashant <prashantrahul141@protonmail.com>
2023-07-25 10:23:29 +0530

    add blob spin animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
   },
   content: ['./templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'blob-spin': {
+          '0%': {
+            rotate: '0deg',
+          },
+          '50%': {
+            scale: '1 1.5',
+          },
+          '100%': {
+            rotate: '360deg',
+          },
+        },
+      },
+      animation: {
+        'blob-spin': 'blob-spin 15s linear infinite',
+      },
+    },
   },
   plugins: [],
 };
