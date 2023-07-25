1bbea3d1a0a632e3fa6afa4b1a7607ea9609ef27
Prashant <prashantrahul141@protonmail.com>
2023-07-25 08:09:48 +0530

    add hover only when supported flag for tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: ['./templates/**/*.html'],
   theme: {
     extend: {},
