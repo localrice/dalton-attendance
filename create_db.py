f7d60158c094e26354f15d143ad708332e543c4a
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-27 12:42:06 +0530

    divided the studentInfo into two different tables

--- create_db.py
+++ create_db.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('dalton.db')
 cursor = conn.cursor()
 
 sql = '''
-CREATE TABLE IF NOT EXISTS StudentInfo (
+CREATE TABLE IF NOT EXISTS StudentInfo11 (
     student_id TEXT PRIMARY KEY, 
     student_name TEXT,
     roll_no INTEGER, 
@@ -17,7 +17,20 @@ CREATE TABLE IF NOT EXISTS StudentInfo (
 
 cursor.execute(sql)
 conn.commit()
+sql = '''
+CREATE TABLE IF NOT EXISTS StudentInfo12 (
+    student_id TEXT PRIMARY KEY, 
+    student_name TEXT,
+    roll_no INTEGER, 
+    stream TEXT, 
+    phone_numbers INTEGER,
+    academic_year_from INTEGER,
+    academic_year_to INTEGER
+)
+'''
 
+cursor.execute(sql)
+conn.commit()
 sql = '''
 CREATE TABLE IF NOT EXISTS dailyAttendance (
     date TEXT,
