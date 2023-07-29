6312ccf20f2bb57030c0cc14e5de894ad3141f74
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-29 15:53:38 +0530

    updated regex to detect the = sign

--- utils/sql_sanitization.py
+++ utils/sql_sanitization.py
@@ -1,11 +1,16 @@
 import re
 
+
+def remove_spaces_from_list(input_list):
+    return [string.replace(' ', '') for string in input_list]
+
+
 sql_keywords = ['drop', 'delete', 'truncate', 'insert', 'update', 'alter', 'create',
                 'select', 'union', 'join', 'or', 'and', 'exec', 'execute', 'xp_',
                 'xp_cmdshell', 'sp_', 'sp_executesql', 'declare', 'cast', 'convert',
                 'script', 'javascript', 'iframe', 'onload', 'alert', 'document',
                 'cookie', 'eval', 'select', 'where', 'having', 'group by', 'order by',
-                'from', 'count', '--', '/*', '*/']
+                'from', 'count', '--', '/*', '*/', '=']
 
 
 def check(name: str):
@@ -14,7 +19,9 @@ def check(name: str):
     returns True if any word found in sql_keywords is found
     returns False if not
     '''
-    name_tokens = re.findall(r'\b\w+=\w+\b|\b\w+\b', name.lower())
+    pattern = r'\b\w+\b|\s*=\s*|\b=\b'
+    name_tokens = remove_spaces_from_list(re.findall(pattern, name.lower()))
+
     print(name_tokens)
     for token in name_tokens:
         if token in sql_keywords:
