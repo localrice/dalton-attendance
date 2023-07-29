b71841b0b86e7c10ab5cec8a27270e83cf9c8314
KeviTheDuck <levihackermangithub@gmail.com>
2023-07-29 15:17:04 +0530

    update: used regex

--- utils/sql_sanitization.py
+++ utils/sql_sanitization.py
@@ -1,21 +1,22 @@
+import re
+
 sql_keywords = ['drop', 'delete', 'truncate', 'insert', 'update', 'alter', 'create',
                 'select', 'union', 'join', 'or', 'and', 'exec', 'execute', 'xp_',
                 'xp_cmdshell', 'sp_', 'sp_executesql', 'declare', 'cast', 'convert',
                 'script', 'javascript', 'iframe', 'onload', 'alert', 'document',
                 'cookie', 'eval', 'select', 'where', 'having', 'group by', 'order by',
-                'from', 'count', '1=1', '--', '/*', '*/']
+                'from', 'count', '--', '/*', '*/']
 
 
-def check(input: str):
+def check(name: str):
     '''
     takes an input and checks for any words that might be used in an sql injection
-    returns True if any word associated with sql injection is found
+    returns True if any word found in sql_keywords is found
     returns False if not
     '''
-    user_input = input.lower()
-    for k in sql_keywords:
-        if k in user_input:
+    name_tokens = re.findall(r'\b\w+=\w+\b|\b\w+\b', name.lower())
+    print(name_tokens)
+    for token in name_tokens:
+        if token in sql_keywords:
             return True
-        else:
-            pass
     return False
